d0d3dcabd5754de61e7bee71986de00e2a825fbf
Alexander Vodyanoy <alex.vodyanoy@blinkreaction.com>
2015-05-25 11:53:44 +0300

    Run drush in background mode.

--- devel_drush.php
+++ devel_drush.php
@@ -1,10 +1,14 @@
 <?php
 
-$site_folder = $_SERVER['DOCUMENT_ROOT'] . '/' . conf_path();
+$command = array();
 // Go to site folder.
-$command = 'cd' . $site_folder . '; ';
+$command[] = 'cd' . $$_SERVER['DOCUMENT_ROOT'] . '/' . conf_path() . ';';
 // Specific drush command.
-$command .= 'drush -v';
+$command[] = 'drush cc all';
+// Run in background mode.
+$command[] = '> /dev/null 2>&1 &';
+$command = implode(' ', $command);
+
 // Call external console function.
 $result = shell_exec($command);
 
